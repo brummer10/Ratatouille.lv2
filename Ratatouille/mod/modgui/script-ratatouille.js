f69462e69ec481fc7887f2089aba0e478eb65413
brummer10 <brummer-@web.de>
2025-01-30 18:54:04 +0100

    Fix issue #39 Long names in mod version breaking UI

--- Ratatouille/mod/modgui/script-ratatouille.js
+++ Ratatouille/mod/modgui/script-ratatouille.js
@@ -5,21 +5,41 @@ function (event, funcs)
     {
         if (value == 'None')
             icon.find ('[rata-role=SlotA]').text ('-- choose a NAM/AIDA-X model --');
+        else
+        {
+            var string = value.split('\\').pop().split('/').pop();
+            icon.find ('[rata-role=SlotA]').text (string.substring(0, 48));
+        }
     }
     function check_neural_model1(icon, value)
     {
         if (value == 'None')
             icon.find ('[rata-role=SlotB]').text ('-- choose a NAM/AIDA-X model --');
+        else
+        {
+            var string = value.split('\\').pop().split('/').pop();
+            icon.find ('[rata-role=SlotB]').text (string.substring(0, 48));
+        }
     }
     function check_irfile(icon, value)
     {
         if (value == 'None')
             icon.find ('[rata-role=IrA]').text ('-- choose an IR file --');
+        else
+        {
+            var string = value.split('\\').pop().split('/').pop();
+            icon.find ('[rata-role=IrA]').text (string.substring(0, 48));
+        }
     }
     function check_irfile1(icon, value)
     {
         if (value == 'None')
             icon.find ('[rata-role=IrB]').text ('-- choose an IR file --');
+        else
+        {
+            var string = value.split('\\').pop().split('/').pop();
+            icon.find ('[rata-role=IrB]').text (string.substring(0, 48));
+        }
     }
     function set_latency(icon, value)
     {
